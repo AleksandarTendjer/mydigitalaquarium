9245914136861a6f7f62dd5378ebbd2383180562
Aleksandar Tendjer <tendjer@protonmail.com>
2025-12-05 23:11:50 +0100

    Added main menu logic

--- mydigitalaquarium/src/app/aquarium/page.tsx
+++ mydigitalaquarium/src/app/aquarium/page.tsx
@@ -1,9 +1,11 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "motion/react";
 
 export default function Aquarium() {
 	return (
-		<div className="w-full min-h-screen grid grid-cols-12 grid-rows-12 bg-linear-to-b from-1% via-blue-300 to-99%">
+		<div className="w-full min-h-screen grid grid-cols-12 grid-rows-12 ">
 			<Link className="w-full row-span-1 col-span-1" href={"/"}>
 				<Image
 					src={"/imgs/back.gif"}
@@ -13,20 +15,20 @@ export default function Aquarium() {
 					className="left-10 align-top items-baseline p-5"
 				/>
 			</Link>
-			<div className=" p-4 m-4 sm:m-6 row-span-11 col-span-11 grid grid-cols-11 grid-rows-11  relative bg-zinc-200/15 rounded-4xl">
+			<motion.div className=" p-4 m-4 sm:m-6 row-span-11 col-span-11 grid grid-cols-11 grid-rows-11  relative  bg-radial-[at_50%] from-[#A9E835]/70 to-95%   rounded-4xl">
 				<div className="col-span-9 row-span-8 bg-[url(/imgs/tankbackground.jpg)] bg-cover bg-center z-10 -mr-32 -mb-15 rounded-4xl "></div>
-				<div className=" w-full col-end-12 col-span-2 row-span-11 bg-[url(/imgs/sidebarAquarium.png)] bg-size-[100%_100%] bg-center bg-no-repeat z-20 grid grid-rows-9 grid-cols-7 ">
-					<button className="rounded-full bg-radial-[at_50%_75%] from-[#A9E835] to-[#012501] col-start-5 col-end-7 row-start-2 row-end-2 h-6/9  border-4 border-[#777777]"></button>
-					<button className="rounded-full bg-radial-[at_50%_75%] from-[#A9E835] to-[#012501] col-start-5 col-end-7 row-start-3 row-end-3 h-6/9 border-4 border-[#777777]"></button>
-					<button className="rounded-full bg-radial-[at_50%_75%] from-[#A9E835] to-[#012501] col-start-5 col-end-7 row-start-4 row-end-4 h-6/9 border-4 border-[#777777]"></button>
-					<button className="rounded-full bg-radial-[at_50%_75%] from-[#A9E835] to-[#012501] col-start-4 col-end-7 row-start-7 row-end-9 h-6/9 border-4 border-[#777777]"></button>
+				<div className=" w-full col-end-12 col-span-2 row-span-11 bg-[url(/imgs/sidebarAquarium1.png)] bg-size-[100%_100%] bg-center bg-no-repeat z-20 grid grid-rows-9 grid-cols-7 ">
+					<button className=" hover:cursor-grab rounded-full bg-radial-[at_50%_75%] from-[#A9E835] to-[#012501] hover:from-[#012501] hover:to-[#A9E835] col-start-5 col-end-7 row-start-2 row-end-2 h-7/9  border-5  border-x-[#777777] border-y-[#f6f6f6cc] shadow-[#2D6B00] shadow-2xl hover:border-y-[#777777]  hover:border-x-[#f6f6f6cc]"></button>
+					<button className=" hover:cursor-grab rounded-full bg-radial-[at_50%_75%] from-[#A9E835] to-[#012501] hover:from-[#012501] hover:to-[#A9E835] col-start-5 col-end-7 row-start-3 row-end-3 h-7/9 border-5  border-x-[#777777] border-y-[#f6f6f6cc] shadow-[#2D6B00] shadow-2xl hover:border-y-[#777777]  hover:border-x-[#f6f6f6cc]"></button>
+					<button className=" hover:cursor-grab rounded-full bg-radial-[at_50%_75%] from-[#A9E835] to-[#012501] hover:from-[#012501] hover:to-[#A9E835] col-start-5 col-end-7 row-start-4 row-end-4 h-7/9 border-5  border-x-[#777777] border-y-[#f6f6f6cc] shadow-[#2D6B00] shadow-2xl hover:border-y-[#777777]  hover:border-x-[#f6f6f6cc]"></button>
+					<button className=" hover:cursor-grab rounded-full bg-radial-[at_50%_75%] from-[#A9E835] to-[#012501] hover:from-[#012501] hover:to-[#A9E835] col-start-4 col-end-7 row-start-7 row-end-9 h-7/9 border-5   border-x-[#777777] border-y-[#f6f6f6cc] shadow-[#2D6B00] shadow-2xl hover:border-y-[#777777]  hover:border-x-[#f6f6f6cc]"></button>
 				</div>
 				<div className="w-full col-span-9 row-span-3 row-end-12 bg-[url(/imgs/downbarAquarium.png)] bg-size-[70%_80%] bg-center bg-no-repeat z-20 flex items-center justify-center">
-					<div className="bg-radial-[at_50%_75%] from-[#394714] to-[#36980F]  text-[#BBD189] rounded-4xl bg-center justify-self-center  bg-contain w-1/3 h-5/9 border-4">
+					<div className="bg-radial-[at_50%_75%] from-[#394714] to-[#36980F]  text-[#BBD189] rounded-4xl bg-center justify-self-center  bg-contain w-1/3 h-5/9 border-4 shadow-[#2D6B00] shadow-2xl">
 						<p className="p-5">This is just a fish</p>
 					</div>
 				</div>
-			</div>
+			</motion.div>
 		</div>
 	);
 }
--- mydigitalaquarium/src/app/page.tsx
+++ mydigitalaquarium/src/app/page.tsx
@@ -1,32 +1,65 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-
+import { motion } from "motion/react";
+import { useState } from "react";
 export default function Home() {
 	const router = useRouter();
-
+	const items = [
+		{ img: "/imgs/diveIntoGif.gif", label: "diveIn", push: "/aquarium" },
+		{ img: "/imgs/friendsGif.gif", label: "friends", push: "/friends" },
+		{ img: "/imgs/aboutGif.gif", label: "about", push: "/about" },
+	];
+	const [rotationOffset, setRotationOffset] = useState(0);
+	const radius = 180;
+	console.log(items.length);
 	return (
-		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
-			<main className="flex min-h-screen  h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16   sm:items-start">
-				<ul className="flex flex-col w-full h-full justify-center items-center">
-					<li className="my-5 w-full h-full">
-						<button
-							className="relative w-full h-full  customCursor bg-linear-to-b from-10% via-zinc-400 to-80% rounded-e-full p-2  hover:from-zinc-400 hover:via-transparent hover:to-zinc-400 "
-							onClick={() => router.push("/aquarium")}>
-							<Image fill src={"/imgs/diveIn.gif"} alt="diveIn" />
-						</button>
-					</li>
-					<li className="my-5 w-full h-full">
-						<button className=" relative w-full h-full hover-friends p-3 bg-linear-to-b from-10% via-zinc-400 to-80% rounded-e-full   hover:from-zinc-400 hover:via-transparent hover:to-zinc-400">
-							<Image fill src={"/imgs/friends.gif"} alt="friends" />
-						</button>
-					</li>
-					<li className=" my-5 w-full h-full">
-						<button className="relative w-full h-full  hover-about bg-linear-to-b from-10% via-zinc-400 to-80% rounded-e-full rounded-3xl p-2 hover:from-zinc-400 hover:via-transparent hover:to-zinc-400">
-							<Image fill src={"/imgs/about.gif"} alt="about" />
-						</button>
-					</li>
-				</ul>
+		<div className="flex min-h-screen items-center justify-center font-sans ">
+			<main className="flex min-h-screen  h-screen w-full max-w-3xl flex-col items-center justify-between    sm:items-start">
+				<motion.div
+					className="relative h-[900px] w-[900px]  rounded-full  bg-radial-[at_50%] from-[#A9E835]/60 to-70% "
+					animate={{
+						scale: [1, 1.08, 1],
+						opacity: [0.8, 1, 0.8],
+					}}
+					transition={{
+						duration: 3,
+						repeat: Infinity,
+						ease: "easeInOut",
+					}}
+					onClick={() => {
+						setRotationOffset((prev) => prev + 1);
+					}}>
+					{items.map((item, i) => {
+						const angle = (360 / items.length) * (i + rotationOffset);
+						const angleRad = angle * (Math.PI / 180);
+						console.log(`angleRad ${angleRad} radius ${radius}`);
+						const isActive = (i + rotationOffset) % items.length === 0;
+
+						return (
+							<motion.div
+								key={i}
+								className="absolute left-1/4 top-2/5 "
+								animate={{
+									x: Math.cos(angleRad) * radius,
+									y: Math.sin(angleRad) * radius,
+									scale: isActive ? 1.3 : 0.8,
+									opacity: isActive ? 1 : 0.5,
+								}}
+								transition={{ duration: 0.3 }}>
+								<button
+									className="relative w-[180px] h-[180px]   bg-linear-to-b from-10% via-zinc-400 to-80% rounded-full p-2  hover:from-zinc-400 hover:via-transparent hover:to-zinc-400 hover:border-2 hover:border-dotted"
+									disabled={!isActive}
+									onClick={(e) => {
+										e.stopPropagation();
+										router.push(item.push);
+									}}>
+									<Image fill src={item.img} alt={item.label} />
+								</button>{" "}
+							</motion.div>
+						);
+					})}
+				</motion.div>
 			</main>
 		</div>
 	);
